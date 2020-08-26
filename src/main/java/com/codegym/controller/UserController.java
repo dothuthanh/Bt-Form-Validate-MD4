ad7a0fd053b268c2f8870db00a70e5b688dd9a16
dothuthanh <thanh.do.codegym@gmail.com>
2020-08-26 14:43:08 +0700

    ok

---
+++ src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import com.codegym.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user")
+    public ModelAndView showForm() {
+        ModelAndView model = new ModelAndView("index");
+        model.addObject("user", new User());
+        return model;
+    }
+
+    @PostMapping("/validateUser")
+    public ModelAndView checkValidity(@Validated @ModelAttribute("user") User user, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            ModelAndView model = new ModelAndView("index");
+            return model;
+        }
+        ModelAndView model = new ModelAndView("result");
+        userService.save(user);
+        return model;
+    }
+}
---
+++ src/main/java/com/codegym/service/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.codegym.service;
+
+import com.codegym.model.User;
+
+public class UserServiceImpl implements UserService {
+
+
+
+    @Override
+    public void save(User user) {
+
+    }
+}
